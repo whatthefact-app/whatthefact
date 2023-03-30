1bb93eca5ed505e55b809f769239e9511f010305
William H <william.hermozo@protonmail.com>
2023-03-30 18:18:06 +0100

    set up tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<slot />
+
+<!-- if need to access tailwind styles in global -->
+<!-- <style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.500);
+	}
+</style> -->
