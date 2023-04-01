e4008f89b1e6727018e4e3fb47ab953eb95c302d
William H <william.hermozo@protonmail.com>
2023-04-01 14:46:39 +0100

    fix select width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,18 +30,18 @@
 
 	<p>You're playing in</p>
 
-	<form class="flex w-full justify-center">
-		<select
-			bind:value={currentCountry}
-			class="grid h-[68px] w-full max-w-sm place-items-center rounded-[29px] bg-white px-6 py-3 font-heading text-[1.6875rem] font-extrabold text-black"
-		>
-			<option value="PT" selected={currentCountry === 'PT'}>{countries.PT}</option>
-			<option value="FR" selected={currentCountry === 'FR'}>{countries.FR}</option>
-			<option value="US" selected={currentCountry === 'US'}>{countries.US}</option>
-			<option value="AU" selected={currentCountry === 'AU'}>{countries.AU}</option>
-			<option value="BR" selected={currentCountry === 'BR'}>{countries.BR}</option>
-		</select>
-	</form>
+	<!-- <form class="flex w-full justify-center"> -->
+	<select
+		bind:value={currentCountry}
+		class="grid h-[68px] w-full place-items-center rounded-[29px] bg-white px-6 py-3 font-heading text-[1.6875rem] font-extrabold text-black"
+	>
+		<option value="PT" selected={currentCountry === 'PT'}>{countries.PT}</option>
+		<option value="FR" selected={currentCountry === 'FR'}>{countries.FR}</option>
+		<option value="US" selected={currentCountry === 'US'}>{countries.US}</option>
+		<option value="AU" selected={currentCountry === 'AU'}>{countries.AU}</option>
+		<option value="BR" selected={currentCountry === 'BR'}>{countries.BR}</option>
+	</select>
+	<!-- </form> -->
 
 	<NavigationButton href={`/${$facts[0].id}`}>Play</NavigationButton>
 </Section>
