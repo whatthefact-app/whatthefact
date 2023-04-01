e9c4139bed3c9bc57a5d8e92c9a053fa1b475cf3
William H <william.hermozo@protonmail.com>
2023-04-01 19:03:28 +0100

    add carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,35 @@
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/environment';
 	import beetle from '$lib/assets/images/beetle.png';
+	import dino from '$lib/assets/images/dino.png';
+	import fish from '$lib/assets/images/fish.png';
+	import flower from '$lib/assets/images/flower.png';
+	import gendarme from '$lib/assets/images/gendarme.png';
+	import parrot from '$lib/assets/images/parrot.png';
+	import peacock from '$lib/assets/images/peacock.png';
+	import shell from '$lib/assets/images/shell.png';
+	import snail from '$lib/assets/images/snail.png';
+	import tree from '$lib/assets/images/tree.png';
+	import whale from '$lib/assets/images/whale.png';
+
 	import PageHeading from '../components/PageHeading.svelte';
 	import { countries } from '../constants/constants';
 	import Section from '../components/Section.svelte';
 
 	export let data: PageData;
+
+	let image: string | null;
+	let images = [beetle, dino, fish, flower, gendarme, parrot, peacock, shell, snail, tree, whale];
+
+	const getRandomImage = () => {
+		const randomIndex = Math.floor(Math.random() * images.length);
+		return images[randomIndex];
+	};
+
+	// get a new random image every 1 second
+	$: setInterval(() => {
+		image = getRandomImage();
+	}, 750);
 
 	let currentCountry: string;
 	let paramCountry = $page.url.searchParams.get('country');
@@ -23,9 +47,21 @@
 </script>
 
 <Section className="gap-8 pt-16">
+	<!-- <div>
+		<h1 class="mt-16 font-heading text-7xl font-black leading-none sm:mt-24">
+			What<br />the<br /><span class="text-purple-500">fact?!</span>
+		</h1>
+
+		<img src={beetle} alt="Beetle" class="mt-16 h-44" />
+	</div> -->
+
 	<PageHeading>Test your knowledge about nature around you.</PageHeading>
 
-	<img src={beetle} alt="Beetle" class="h-28" />
+	<div class="h-28">
+		{#if image}
+			<img src={image} alt="Beetle" class="h-28" />
+		{/if}
+	</div>
 
 	<p>You're playing in</p>
 
